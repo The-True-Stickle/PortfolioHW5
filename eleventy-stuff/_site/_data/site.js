7827a8a480b4688e4e790ecedeb9e1dbf7694825
Stickle <64675913+The-True-Stickle@users.noreply.github.com>
2026-08-02 19:21:20 -0700

    Fixed output directory name

---
+++ eleventy-stuff/_site/_data/site.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: "Bowen (Bee) Bothello's Website",
+    author: "Bowen Bothello",
+    year: 2026,
+
+    navigation: [
+        {label: "Home", url: "/"},
+        {label: "Deitrus", url: "/projects/Deitrus"},
+        {label: "Overtime", url: "/projects/Overtime"},
+        {label: "Threes Will Die", url: "/projects/Threes"}
+    ]
+}
